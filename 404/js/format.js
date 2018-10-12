80a53a818ed7674ed74f89f70f9fcb35d6442749
Dan Lee <dan-lee1@hotmail.co.uk>
2018-10-12 20:38:59 +0100

    Update 2.0.1

---
+++ 404/js/format.js
@@ -0,0 +1,13 @@
+var text = ["> Formatting C Drive.", "> Formatting C Drive..", "> Formatting C Drive..."];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 1000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
