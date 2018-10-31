24276f3033cff462946e84561468cddeff40bf9a
Dan Lee <dan-lee1@hotmail.co.uk>
2018-10-31 22:39:08 +0000

    beta - intranet

---
+++ intranet/beta/assets/js/news.js
@@ -0,0 +1,7 @@
+function load() {
+var feed ="http://feeds.bbci.co.uk/news/world/rss.xml";
+new GFdynamicFeedControl(feed, "feedControl");
+
+}
+google.load("feeds", "1");
+google.setOnLoadCallback(load);
