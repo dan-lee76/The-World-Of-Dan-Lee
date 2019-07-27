03bd7560781eaa4b224e89794602a6cdcae4c9d7
Dan Lee <dan-lee1@hotmail.co.uk>
2019-07-27 21:37:32 +0100

    beta

--- beta/beta beta/new project/assets/js/apis.js
+++ beta/beta beta/new project/assets/js/apis.js
@@ -84,8 +84,3 @@ function myClick() {
 }
 myClick();
 
-// When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
